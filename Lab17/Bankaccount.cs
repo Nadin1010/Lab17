83fb405e2f02c9c79646d768cf5c69c453ea3430
Надежда Кузнецова <kuznecovanadezhd@gmail.com>
2022-01-20 20:07:46 +0300

    Добавьте файлы проекта.

---
+++ Lab17/Bankaccount.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab17
+{
+    class Bankaccount<T>
+    {
+        public T Num { get; set; }
+        public string Name { get; set; }
+        public double Summa { get; set; }
+
+        //public Bankaccount <T> (T num, string name, double summa)
+        //{
+        //    Num = num;
+        //    Name = name;
+        //    Summa = summa;
+        //}
+
+        public string GetInfo()
+        {
+            return $"{Num} {Name} {Summa}";
+        }
+
+        public void Input()
+        {
+            Console.WriteLine("Введите номер счета");
+            string stringNum = Console.ReadLine();
+            Num = (T)Convert.ChangeType(Num, typeof(T));
+
+            Console.WriteLine("Введите имя владельца");
+            Name = Console.ReadLine();
+            Console.WriteLine("Введите баланс");
+            Summa = Convert.ToDouble(Console.ReadLine());
+        }
+    }
+}
+    
---
+++ Lab17/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Введите номер счета");
+            //string num = Console.ReadLine();
+            //Console.WriteLine("Введите имя владельца");
+            //string name = Console.ReadLine();
+            //Console.WriteLine("Введите баланс");
+            //double summa = Convert.ToDouble(Console.ReadLine());
+            //Bankaccount<int> bankaccount1 = new Bankaccount<int>(num, name, summa);
+            //Console.WriteLine(bankaccount1.GetInfo());
+
+            //Console.WriteLine("Введите номер счета");
+            //string numString = Console.ReadLine();
+            //Console.WriteLine("Введите имя владельца");
+            //name = Console.ReadLine();
+            //Console.WriteLine("Введите баланс");
+            //summa = Convert.ToDouble(Console.ReadLine());
+            //Bankaccount<string> bankaccount2 = new Bankaccount<string>(num, name, summa);
+            //Console.WriteLine(bankaccount1.GetInfo());
+
+            Bankaccount<int> bankaccount1 = new Bankaccount<int>();
+            bankaccount1.Input();
+            Console.WriteLine(bankaccount1.GetInfo());
+
+            Bankaccount<string> bankaccount2 = new Bankaccount<string>();
+            bankaccount2.Input();
+            Console.WriteLine(bankaccount2.GetInfo());
+
+            Console.ReadKey();
+        }
+    }
+}
+    
